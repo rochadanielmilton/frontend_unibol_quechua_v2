895e17cdbff3996a3dcaa94defcb0e128acc789d
johnny-aras <89922953+johnny-aras@users.noreply.github.com>
2024-03-19 11:00:27 -0400

    ffhhcc

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
+let BASE_URL = import.meta.env.VITE_BASE_URL;
+
 const authApi = axios.create({
     //baseURL:'http://127.0.0.1:8000',
-    baseURL:'http://192.168.30.9:8000',
+    //baseURL:'http://192.168.30.9:8000',
+    baseURL:BASE_URL,
     params:{
     }
 })
