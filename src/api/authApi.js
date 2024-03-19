aaab5d76fbfce87128af37b93d8bd9219a498d14
johnny-aras <89922953+johnny-aras@users.noreply.github.com>
2024-03-19 10:58:11 -0400

    cchhd

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const authApi = axios.create({
-    baseURL:'http://127.0.0.1:8000',
+    //baseURL:'http://127.0.0.1:8000',
+    baseURL:'http://192.168.30.9:8000',
     params:{
     }
 })
